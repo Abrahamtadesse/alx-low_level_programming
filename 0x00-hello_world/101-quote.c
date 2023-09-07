fb170965b553c8e1d6cb52f0d9b411015b08ec3b
Abrahamtadesse <abrahamtadese83@gmail.com>
2023-09-07 14:12:56 -0700

    4-puts.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,19 +2,22 @@
 #include <string.h>
 #include <errno.h>
 
+/**
+ *main - the main function
+ *
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = strlen(message);
-    ssize_t ret;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = strlen(message);
+ssize_t ret;
 
-    ret = write(2, message, len);
-    if (ret != len) {
-        int error = (ret == -1) ? errno : EIO;
-        const char *errorMsg = strerror(error);
-        write(2, errorMsg, strlen(errorMsg));
-        return 1;
+ret = write(2, message, len);
+if (ret != len)
+{
+int error = (ret == -1) ? errno : EIO;
+const char *errorMsg = strerror(error);
+write(2, errorMsg, strlen(errorMsg));
+return 1;
     }
-
-    return (1);
+return (1);
 } 
