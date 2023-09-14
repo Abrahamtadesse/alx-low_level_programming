5c277a6b19a9e4b7ea44430da139926984ef8a50
Abrahamtadesse <abrahamtadese83@gmail.com>
2023-09-14 14:26:36 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,9 +2,21 @@
 
 int main(void)
 {
-	int n;
+	unsigned long int t1 = 1;
+	unsigned long int t2 = 2;
+	unsigned long int tn;
+	int i;
 
-	for (n = 1 ; n <= 100 ; n = n + n)
-		printf("%d, ", n);
+	printf("%lu, ", t1);
+	for (i = 1 ; i < 50 ; i++)
+	{
+		printf("%lu", t2);
+		tn = t1 + t2;
+		t1 = t2;
+		t2 = tn;
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n");
 	return (0);
 }
