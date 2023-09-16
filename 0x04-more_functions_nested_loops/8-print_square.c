c27ee23a1514790d661251e99cee843d719dbe59
Abrahamtadesse <abrahamtadese83@gmail.com>
2023-09-16 03:26:54 -0700

    _putchar

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints square using a character
+ * @size: size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 1 ; j <= size ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
