352598761d2a3b1f13c1a8b78a5f44f23d3df224
Abrahamtadesse <abrahamtadese83@gmail.com>
2023-09-16 01:11:39 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int main(void)
 	for (n = 1 ; n <= 100 ; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+			printf("%s", "FizzBuzz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf("%s", "Buzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("%s", "Fizz");
 		else
-			printf("%d ", n);
+			printf("%d", n);
 		if (n != 100)
 			printf(" ");
 	}
